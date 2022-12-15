29799e494a4e35ac86a88b4caa6c6c5ba04ffbd2
gustav <gustavbadie@gmail.com>
2022-12-15 17:20:26 +0330

    add utc time

--- utils/data/fake_generator.py
+++ utils/data/fake_generator.py
@@ -1,4 +1,5 @@
-import random
+from datetime import timezone
+import datetime
 
 import pandas as pd
 from faker import Faker
@@ -16,7 +17,9 @@ def generate_users(rows_count: int = 10000, write_to_file: bool = False) -> pd.D
         name = faker.name()
         gender = faker.word(1, genders)
         age = faker.random_int(10, 100)
-        date_joined = faker.date_between()
+        date_joined = datetime.datetime.fromisoformat(faker.iso8601())
+        utc_time = date_joined.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
         marriage = faker.word(1, marriage_status)
         balance = faker.random_int(0, 100000)
         id = faker.md5()
@@ -26,7 +29,7 @@ def generate_users(rows_count: int = 10000, write_to_file: bool = False) -> pd.D
             'name': name,
             'gender': gender,
             'age': age,
-            'date_joined': date_joined,
+            'date_joined': utc_timestamp,
             'marriage_status': marriage,
             'balance': balance,
             'country': country,
@@ -40,37 +43,43 @@ def generate_interactions(user_ids: list, target_ids: list, rows_count: int = 10
                           write_to_file: bool = False) -> pd.DataFrame:
     result: pd.DataFrame = pd.DataFrame()
     interaction_types = ['comment', 'liked', 'bought', 'added_to_wish_list', 'followed', 'opened', 'shared',
-                         'revisited',
-                         'removed_from_wish_list']
+                         'revisited']
 
     for index in range(rows_count * len(interaction_types)):
         user_id = faker.word(1, user_ids)
         interaction_type = faker.word(1, interaction_types)
+        time = datetime.datetime.fromisoformat(faker.iso8601())
+        utc_time = time.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
         target_id = faker.word(1, target_ids)
-        value = bool(random.getrandbits(1))
         result = pd.concat([result, pd.DataFrame({
             'user_id': user_id,
             'interaction_type': interaction_type,
             'target_id': target_id,
-            'value': value,
+            'time': utc_timestamp,
         }, index=[0])])
     if write_to_file:
         write('csv', 'interactions_dataset.csv', result)
     return result
 
 
-def generate_reviews(reviews: pd.DataFrame, user_ids: list, target_ids: list, write_to_file: bool = False) -> pd.DataFrame:
+def generate_reviews(reviews: pd.DataFrame, user_ids: list, target_ids: list,
+                     write_to_file: bool = False) -> pd.DataFrame:
     result = pd.DataFrame()
     for index, row in reviews.iterrows():
         user_id = faker.word(1, user_ids)
         target_id = faker.word(1, target_ids)
         text = row['Review Text']
+        time = datetime.datetime.fromisoformat(faker.iso8601())
+        utc_time = time.replace(tzinfo=timezone.utc)
+        utc_timestamp = utc_time.timestamp()
         rate = row['Rating']
         result = pd.concat([result, pd.DataFrame({
             'user_id': user_id,
             'target_id': target_id,
             'text': text,
             'rate': rate,
+            'time': utc_timestamp,
         }, index=[0])])
     if write_to_file:
         write('csv', 'reviews_dataset.csv', result)
