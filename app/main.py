6b0ad3c1d6dd4dfcf2522ea1e68c09398c0b0185
gustav <gustavbadie@gmail.com>
2022-12-19 23:14:17 +0330

    implement collaborative model

--- app/main.py
+++ app/main.py
@@ -5,12 +5,15 @@ from utils.io.loader import load
 
 def main():
     initialize_settings()
-    data = load('csv', 'shoes_dataset.csv', write_to_file=False)
+    data = load('csv', 'preprocessed_shoes.csv', write_to_file=False)
     rows_count = data.shape[0]
-    users = load('csv', 'users_dataset.csv', write_to_file=False)
-    interactions = load('csv', 'interactions_dataset.csv', write_to_file=False)
+    # users = load('csv', 'users_dataset.csv', write_to_file=False)
+    # interactions = load('csv', 'interactions_dataset.csv', write_to_file=False)
     raw_reviews = load('csv', 'raw_reviews_dataset.csv', write_to_file=False)
-    reviews = load('csv', 'reviews_dataset.csv', write_to_file=False)
+    # reviews = load('csv', 'reviews_dataset.csv', write_to_file=False)
+    users = generate_users(rows_count, True)
+    generate_interactions(users['id'], data['asin'], rows_count, True)
+    generate_reviews(raw_reviews, users['id'], data['asin'], True)
 
 
 if __name__ == "__main__":
